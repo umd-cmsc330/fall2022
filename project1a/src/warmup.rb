10a24b32a2bf4d04351237c56fe073ee04c39297
mson3476 <112913205+mson3476@users.noreply.github.com>
2022-09-06 23:34:47 -0400

    update

--- project1a/src/warmup.rb
+++ project1a/src/warmup.rb
@@ -3,17 +3,20 @@ def fib(n)
     i = 0
 
     while i < n
-        if i == 0 || i == 1
+        if i == 0
+            arr[i] = 0
+        elsif i == 1
             arr[i] = 1
         else
-            arr[i] = arr[i - 1] + arr[1 - 2]
+            arr[i] = arr[i - 1] + arr[i - 2]
         end  
 
-        i++
+        i += 1
     end
-  
+
     arr
 end
+
 
 def isPalindrome(n)
     number = n.to_s
@@ -28,28 +31,27 @@ end
 
 
 def nthmax(n, a)
-    a.sort
-    a.reverse
-
-    a[n]
+    arr = a.sort
+    arr.reverse!
+    arr[n]
 end
 
 
 def freq(s)
-    hash = {}
+    hash = Hash.new
     chars = s.split('')
-    answer = nil
+    answer = ""
     answer_count = 0
 
     if s == ""
         return ""
     end
-    
-    for i in chars do
+
+    chars.each do |i|
         if hash.key?(i) == true
-            hash[i]++
+            hash[i] += 1
         else
-            hash[:i] = 1
+            hash[i] = 1
         end
 
         if hash[i] > answer_count
@@ -63,10 +65,36 @@ end
 
 
 def zipHash(arr1, arr2)
-    
+    if arr1.length != arr2.length
+        return nil
+    end
+
+    hash = Hash.new
+    i = 0
+
+    while i < arr1.length
+        hash[arr1[i]] = arr2[i]
+        i += 1
+    end
+
+    hash
 end
 
 
 def hashToArray(hash)
-    raise Exception, "Not Implemented"
+    arr = Array.new(hash.size) {Array.new(2)}
+    i = 0
+
+    if hash == nil
+        return []
+    end
+
+    hash.each do |k, v|
+        arr[i][0] = k
+        arr[i][1] = v
+        i += 1
+    end
+
+    arr
 end
+
